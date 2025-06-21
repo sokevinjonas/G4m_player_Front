c1c40bf58eb712025e7c29b3609e139ff0d08d71
sokevinjonas <sokevin7@gmail.com>
2025-06-21 19:13:17 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'G4m_player_Front',
+  webDir: 'www'
+};
+
+export default config;
