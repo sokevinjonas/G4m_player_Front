7a17df1d905cbc49a3fea30e5431092ecfc1ade6
sokevinjonas <sokevin7@gmail.com>
2025-06-22 06:29:26 +0000

    feat: implement game filtering and category selection; add loading skeleton for games list

--- src/app/games/games.page.ts
+++ src/app/games/games.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../core/services/api/api.service';
 
 @Component({
   selector: 'app-games',
@@ -7,7 +8,35 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class GamesPage implements OnInit {
-  constructor() {}
+  games: any[] = [];
+  filteredGames: any[] = [];
+  categories: string[] = [];
+  selectedCategory: string = 'Tous';
 
-  ngOnInit() {}
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getGames().subscribe((data) => {
+      this.games = data;
+      this.filteredGames = data;
+      // Extraire les catégories uniques
+      const cats = data
+        .map((g: any) => g.type_game?.name)
+        .filter((c: string | undefined) => !!c);
+      this.categories = ['Tous', ...Array.from(new Set<string>(cats))];
+      console.log(this.games);
+    });
+  }
+
+  selectCategory(category: string | undefined) {
+    if (!category) return;
+    this.selectedCategory = category;
+    if (category === 'Tous') {
+      this.filteredGames = this.games;
+    } else {
+      this.filteredGames = this.games.filter(
+        (g) => g.type_game?.name === category
+      );
+    }
+  }
 }
