3e6dd9847e2571f173ab00109a4b1d4f4d9947d4
sokevinjonas <sokevin7@gmail.com>
2025-06-28 20:22:52 +0000

    feat: add clipboard functionality to copy referral code and update navigation logic

--- src/app/pages/parrainage/parrainage.page.ts
+++ src/app/pages/parrainage/parrainage.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Clipboard } from '@capacitor/clipboard';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-parrainage',
   templateUrl: './parrainage.page.html',
@@ -7,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class ParrainagePage implements OnInit {
-  constructor() {}
+  user: any = {};
+  constructor(private toastController: ToastController) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('user') || '{}');
+    console.log('User:', this.user);
+  }
+
+  writeToClipboard = async () => {
+    await Clipboard.write({
+      string: this.user?.referral_code,
+    });
+    console.log(
+      'Code de parrainage copié dans le presse-papiers:',
+      this.user?.referral_code
+    );
+
+    // Afficher le toast de confirmation
+    const toast = await this.toastController.create({
+      message: 'Code de parrainage copié avec succès !',
+      duration: 2000,
+      position: 'bottom',
+      color: 'success',
+      icon: 'checkmark-circle',
+    });
+    toast.present();
+  };
 }
